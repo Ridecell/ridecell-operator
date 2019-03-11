478ae4d5795609d24ba8d7fd6fac2a451aab9b6c
dhouti <l.rex.via@gmail.com>
2019-03-11 13:43:42 -0700

    Added aws cleanup script to run after integration tests

--- scripts/aws_cleanup/delete_operator_resources.go
+++ scripts/aws_cleanup/delete_operator_resources.go
@@ -6,9 +6,11 @@ import (
 	"regexp"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/pkg/errors"
 )
 
 func main() {
@@ -20,23 +22,27 @@ func main() {
 	stssvc := sts.New(sess)
 	getCallerIdentityOutput, err := stssvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
 	if err != nil {
-		panic(err)
+		exitWithError(err)
 	}
 	if aws.StringValue(getCallerIdentityOutput.Account) != os.Getenv("AWS_TESTING_ACCOUNT_ID") {
-		fmt.Printf("This cleanup script is only permitted to run on the testing account.\n")
-		os.Exit(1)
+		exitWithError(errors.New("this cleanup script is only permitted to run on the testing account"))
 	}
 
 	s3svc := s3.New(sess)
 	s3BucketsToDeleteOutput, err := getS3BucketsToDelete(s3svc)
 	if err != nil {
-		panic(err)
+		exitWithError(err)
+	}
+
+	// If there are a ton of results something bad happened
+	if len(s3BucketsToDeleteOutput) > 10 {
+		exitWithError(errors.New("more than ten buckets to delete, aborting"))
 	}
 
 	for _, s3BucketToDelete := range s3BucketsToDeleteOutput {
 		err = deleteS3Bucket(s3svc, s3BucketToDelete)
 		if err != nil {
-			panic(err)
+			exitWithError(err)
 		}
 	}
 }
@@ -56,9 +62,13 @@ func getS3BucketsToDelete(s3svc *s3.S3) ([]*string, error) {
 			getBucketTaggingOutput, err := s3svc.GetBucketTagging(&s3.GetBucketTaggingInput{
 				Bucket: bucket.Name,
 			})
-			if err != nil {
+			if ec2err, ok := err.(awserr.Error); ok && ec2err.Code() == "NoSuchTagSet" {
+				// There is no tag set associated with the bucket.
+				getBucketTaggingOutput = &s3.GetBucketTaggingOutput{TagSet: []*s3.Tag{}}
+			} else if err != nil {
 				return nil, err
 			}
+
 			for _, tagSet := range getBucketTaggingOutput.TagSet {
 				if aws.StringValue(tagSet.Key) == "ridecell-operator" {
 					bucketsToDelete = append(bucketsToDelete, bucket.Name)
@@ -71,9 +81,14 @@ func getS3BucketsToDelete(s3svc *s3.S3) ([]*string, error) {
 }
 
 func deleteS3Bucket(s3svc *s3.S3, bucketname *string) error {
-	// There should not be any items in the bucket from testing.
+	//There should not be any items in the bucket from testing.
 	// If there are items in the bucket this will fail.
 	// Buckets cannot be deleted if there are objects in them.
 	_, err := s3svc.DeleteBucket(&s3.DeleteBucketInput{Bucket: bucketname})
 	return err
+}
+
+func exitWithError(err error) {
+	fmt.Printf("%s\n", err.Error())
+	os.Exit(1)
 }
