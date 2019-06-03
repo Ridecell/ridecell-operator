7775b5f306865b10a285e1f44e8a32428e528c6b
dhouti <l.rex.via@gmail.com>
2019-06-03 13:01:33 -0700

    move setup/teardown steps out of suite

--- pkg/controller/rdssnapshot/controller_suite_test.go
+++ pkg/controller/rdssnapshot/controller_suite_test.go
@@ -17,31 +17,17 @@ limitations under the License.
 package rdssnapshot_test
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"fmt"
-	"os"
 	"testing"
-	"time"
 
 	"github.com/onsi/ginkgo"
 	"github.com/onsi/gomega"
 
 	"github.com/Ridecell/ridecell-operator/pkg/controller/rdssnapshot"
 	"github.com/Ridecell/ridecell-operator/pkg/test_helpers"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds"
-	"github.com/aws/aws-sdk-go/service/sts"
-	"github.com/pkg/errors"
 
-	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
-
-var testHelpers *test_helpers.TestHelpers
-var rdssvc *rds.RDS
-var rdsInstanceID *string
 
 func TestTemplates(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
@@ -49,96 +35,16 @@ func TestTemplates(t *testing.T) {
 }
 
 var _ = ginkgo.BeforeSuite(func() {
-	if os.Getenv("AWS_TESTING_ACCOUNT_ID") == "" {
-		Skip("$AWS_TESTING_ACCOUNT_ID not set, skipping rds integration tests")
-	}
-
-	if os.Getenv("AWS_SUBNET_GROUP_NAME") == "" {
-		panic("$AWS_SUBNET_GROUP_NAME not set, failing test")
-	}
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-1"),
-	})
-	Expect(err).ToNot(HaveOccurred())
-
-	// Check if this being run on the testing account
-	stssvc := sts.New(sess)
-	getCallerIdentityOutput, err := stssvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
-	Expect(err).NotTo(HaveOccurred())
-	if aws.StringValue(getCallerIdentityOutput.Account) != os.Getenv("AWS_TESTING_ACCOUNT_ID") {
-		panic("These tests should only be run on the testing account.")
-	}
-
-	rdssvc = rds.New(sess)
-
-	randOwnerPrefix := os.Getenv("RAND_OWNER_PREFIX")
-	if randOwnerPrefix == "" {
-		panic("$RAND_OWNER_PREFIX not set, failing test")
-	}
-
-	rdsInstanceName := fmt.Sprintf("%s-snapshot-controller", randOwnerPrefix)
-	rdsInstanceID, err = setupRDSInstance(rdsInstanceName)
-	Expect(err).ToNot(HaveOccurred())
-
 	testHelpers = test_helpers.Start(rdssnapshot.Add, false)
 })
 
 var _ = ginkgo.AfterSuite(func() {
 	testHelpers.Stop()
-	err := deleteDBInstance()
-	Expect(err).ToNot(HaveOccurred())
-})
-
-func setupRDSInstance(rdsInstanceName string) (*string, error) {
+	// If the database exists delete it
 	if rdsInstanceID != nil {
-		return rdsInstanceID, nil
-	}
-
-	rawPassword := make([]byte, 32)
-	rand.Read(rawPassword)
-	password := make([]byte, base64.RawURLEncoding.EncodedLen(32))
-	base64.RawURLEncoding.Encode(password, rawPassword)
-
-	_, err := rdssvc.CreateDBInstance(&rds.CreateDBInstanceInput{
-		StorageType:          aws.String("gp2"),
-		AllocatedStorage:     aws.Int64(100),
-		DBInstanceClass:      aws.String("db.t3.micro"),
-		Engine:               aws.String("postgres"),
-		DBInstanceIdentifier: aws.String(rdsInstanceName),
-		MasterUsername:       aws.String("test_rds"),
-		MasterUserPassword:   aws.String(string(password)),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	for true {
-		time.Sleep(time.Minute)
-		describeDBInstancesOutput, err := rdssvc.DescribeDBInstances(&rds.DescribeDBInstancesInput{
-			DBInstanceIdentifier: aws.String(rdsInstanceName),
+		_, err := rdssvc.DeleteDBInstance(&rds.DeleteDBInstanceInput{
+			DBInstanceIdentifier: rdsInstanceID,
 		})
-		if err != nil {
-			return nil, err
-		}
-		dbStatus := aws.StringValue(describeDBInstancesOutput.DBInstances[0].DBInstanceStatus)
-		if dbStatus == "available" {
-			rdsInstanceID = describeDBInstancesOutput.DBInstances[0].DBInstanceIdentifier
-			return nil, nil
-		}
-		if dbStatus == "error" {
-			return nil, errors.New("rds instance in an error state")
-		}
+		Expect(err).ToNot(HaveOccurred())
 	}
-	return &rdsInstanceName, nil
-}
-
-func deleteDBInstance() error {
-	_, err := rdssvc.DeleteDBInstance(&rds.DeleteDBInstanceInput{
-		DBInstanceIdentifier: rdsInstanceID,
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
+})
--- pkg/controller/rdssnapshot/controller_test.go
+++ pkg/controller/rdssnapshot/controller_test.go
@@ -18,11 +18,18 @@ package rdssnapshot_test
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/Ridecell/ridecell-operator/pkg/test_helpers"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/rds"
+	"github.com/aws/aws-sdk-go/service/sts"
+	"github.com/pkg/errors"
 
 	dbv1beta1 "github.com/Ridecell/ridecell-operator/pkg/apis/db/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -32,17 +39,51 @@ import (
 )
 
 var rdsSnapshot *dbv1beta1.RDSSnapshot
+var testHelpers *test_helpers.TestHelpers
+var rdssvc *rds.RDS
+var rdsInstanceID *string
 
 var _ = Describe("rds controller", func() {
 	var helpers *test_helpers.PerTestHelpers
 
 	BeforeEach(func() {
+		if os.Getenv("AWS_TESTING_ACCOUNT_ID") == "" {
+			Skip("$AWS_TESTING_ACCOUNT_ID not set, skipping rdssnapshot integration tests")
+		}
+
+		if os.Getenv("AWS_SUBNET_GROUP_NAME") == "" {
+			panic("$AWS_SUBNET_GROUP_NAME not set, failing test")
+		}
+
+		sess, err := session.NewSession(&aws.Config{
+			Region: aws.String("us-west-1"),
+		})
+		Expect(err).ToNot(HaveOccurred())
+
+		// Check if this being run on the testing account
+		stssvc := sts.New(sess)
+		getCallerIdentityOutput, err := stssvc.GetCallerIdentity(&sts.GetCallerIdentityInput{})
+		Expect(err).NotTo(HaveOccurred())
+		if aws.StringValue(getCallerIdentityOutput.Account) != os.Getenv("AWS_TESTING_ACCOUNT_ID") {
+			panic("These tests should only be run on the testing account.")
+		}
+
+		rdssvc = rds.New(sess)
+
+		randOwnerPrefix := os.Getenv("RAND_OWNER_PREFIX")
+		if randOwnerPrefix == "" {
+			panic("$RAND_OWNER_PREFIX not set, failing test")
+		}
+
+		rdsInstanceName := fmt.Sprintf("%s-snapshot-controller", randOwnerPrefix)
+		rdsInstanceID, err = setupRDSInstance(rdsInstanceName)
+		Expect(err).ToNot(HaveOccurred())
 		helpers = testHelpers.SetupTest()
 
 		rdsSnapshot = &dbv1beta1.RDSSnapshot{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "test",
-				Namespace: "snapshot-controller-test",
+				Namespace: "snapshot-controller",
 			},
 			Spec: dbv1beta1.RDSSnapshotSpec{
 				RDSInstanceID: *rdsInstanceID,
@@ -59,7 +100,8 @@ var _ = Describe("rds controller", func() {
 
 	It("creates a snapshot with no ttl", func() {
 		c := helpers.TestClient
-		c.Create(rdsSnapshot)
+		rdsSnapshot.Name =
+			c.Create(rdsSnapshot)
 
 		fetchSnapshot := &dbv1beta1.RDSSnapshot{}
 		c.EventuallyGet(helpers.Name(rdsSnapshot.Name), fetchSnapshot, c.EventuallyStatus(dbv1beta1.StatusCreating))
@@ -101,4 +143,48 @@ func snapshotExists() bool {
 		return false
 	}
 	return true
+}
+
+// Setup/teardown
+func setupRDSInstance(rdsInstanceName string) (*string, error) {
+	if rdsInstanceID != nil {
+		return rdsInstanceID, nil
+	}
+
+	rawPassword := make([]byte, 32)
+	rand.Read(rawPassword)
+	password := make([]byte, base64.RawURLEncoding.EncodedLen(32))
+	base64.RawURLEncoding.Encode(password, rawPassword)
+
+	_, err := rdssvc.CreateDBInstance(&rds.CreateDBInstanceInput{
+		StorageType:          aws.String("gp2"),
+		AllocatedStorage:     aws.Int64(100),
+		DBInstanceClass:      aws.String("db.t3.micro"),
+		Engine:               aws.String("postgres"),
+		DBInstanceIdentifier: aws.String(rdsInstanceName),
+		MasterUsername:       aws.String("test_rds"),
+		MasterUserPassword:   aws.String(string(password)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for true {
+		time.Sleep(time.Minute)
+		describeDBInstancesOutput, err := rdssvc.DescribeDBInstances(&rds.DescribeDBInstancesInput{
+			DBInstanceIdentifier: aws.String(rdsInstanceName),
+		})
+		if err != nil {
+			return nil, err
+		}
+		dbStatus := aws.StringValue(describeDBInstancesOutput.DBInstances[0].DBInstanceStatus)
+		if dbStatus == "available" {
+			rdsInstanceID = describeDBInstancesOutput.DBInstances[0].DBInstanceIdentifier
+			return nil, nil
+		}
+		if dbStatus == "error" {
+			return nil, errors.New("rds instance in an error state")
+		}
+	}
+	return &rdsInstanceName, nil
 }
