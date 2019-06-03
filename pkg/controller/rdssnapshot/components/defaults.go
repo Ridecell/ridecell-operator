679273bf55ec3b768837de3eec1b050a0bddbcb6
dhouti <l.rex.via@gmail.com>
2019-06-03 13:01:05 -0700

    default snapshotID from object.name

--- pkg/controller/rdssnapshot/components/defaults.go
+++ pkg/controller/rdssnapshot/components/defaults.go
@@ -46,7 +46,7 @@ func (comp *defaultsComponent) Reconcile(ctx *components.ComponentContext) (comp
 	if instance.Spec.SnapshotID == "" {
 		creationTimestamp := instance.ObjectMeta.CreationTimestamp.Add(time.Second * 0)
 		curTimeString := time.Time.Format(creationTimestamp, CustomTimeLayout)
-		instance.Spec.SnapshotID = fmt.Sprintf("%s-%s", instance.Spec.RDSInstanceID, curTimeString)
+		instance.Spec.SnapshotID = fmt.Sprintf("%s-%s", instance.name, curTimeString)
 	}
 
 	return components.Result{}, nil
