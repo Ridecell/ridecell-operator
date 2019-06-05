d49a27e57d34af2868ca9c356fa7d056bbde6c7f
dhouti <l.rex.via@gmail.com>
2019-06-05 13:06:51 -0700

    Delete snapshots with random name prefix

--- scripts/aws_cleanup/delete_operator_resources.go
+++ scripts/aws_cleanup/delete_operator_resources.go
@@ -400,7 +400,7 @@ func deleteParameterGroup(rdssvc *rds.RDS, parameterGroupName *string) error {
 }
 
 func getSnapshotsToDelete(rdssvc *rds.RDS, prefix string) ([]*string, error) {
-	regexString := fmt.Sprintf(`^(%s-test-rds-.*|%s-snapshot-controller-.*)`, prefix, prefix)
+	regexString := fmt.Sprintf(`^%s-.*`, prefix)
 	var snapshotsToDelete []*string
 	err := rdssvc.DescribeDBSnapshotsPages(&rds.DescribeDBSnapshotsInput{}, func(page *rds.DescribeDBSnapshotsOutput, lastPage bool) bool {
 		for _, snapshot := range page.DBSnapshots {
