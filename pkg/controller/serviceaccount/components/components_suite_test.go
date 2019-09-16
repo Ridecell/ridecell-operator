49a17510283618e97b5e983d40d5078363b031aa
Marina Ma <marina@ridecell.com>
2019-09-16 12:17:54 -0700

    fix lint complaint for err handling

--- pkg/controller/serviceaccount/components/components_suite_test.go
+++ pkg/controller/serviceaccount/components/components_suite_test.go
@@ -34,8 +34,9 @@ var instance *gcpv1beta1.GCPServiceAccount
 var ctx *components.ComponentContext
 
 func TestComponents(t *testing.T) {
-	apis.AddToScheme(scheme.Scheme)
 	gomega.RegisterFailHandler(ginkgo.Fail)
+	err := apis.AddToScheme(scheme.Scheme)
+        gomega.Expect(err).NotTo(gomega.HaveOccurred())
 	ginkgo.RunSpecs(t, "serviceaccount Components Suite @unit")
 }
 
