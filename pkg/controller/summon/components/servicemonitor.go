0d6b76cc51d0633ff9dee58239b9222d0b5099c6
dhouti <l.rex.via@gmail.com>
2019-10-16 15:24:56 -0700

    Add delete when not enabled

--- pkg/controller/summon/components/servicemonitor.go
+++ pkg/controller/summon/components/servicemonitor.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2019 Ridecell, Inc.
+Copyright 2019 Ridecell, Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,12 +17,15 @@ limitations under the License.
 package components
 
 import (
+	"fmt"
 	"github.com/Ridecell/ridecell-operator/pkg/components"
 	"github.com/pkg/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 
 	summonv1beta1 "github.com/Ridecell/ridecell-operator/pkg/apis/summon/v1beta1"
 	promv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 )
 
 type serviceMonitorComponent struct {
@@ -44,8 +47,21 @@ func (comp *serviceMonitorComponent) IsReconcilable(ctx *components.ComponentCon
 func (comp *serviceMonitorComponent) Reconcile(ctx *components.ComponentContext) (components.Result, error) {
 	instance := ctx.Top.(*summonv1beta1.SummonPlatform)
 
-	// If our flag is not set or is false exit early
+	// If our flag is not set or is false attempt to delete the object and move on
 	if instance.Spec.Metrics.Web == nil || !*instance.Spec.Metrics.Web {
+		existing := &promv1.ServiceMonitor{}
+		err := ctx.Get(ctx.Context, types.NamespacedName{Name: fmt.Sprintf("%s-web", instance.Name), Namespace: instance.Namespace}, existing)
+		if err != nil {
+			if kerrors.IsNotFound(err) {
+				return components.Result{}, nil
+			}
+			return components.Result{}, errors.Wrap(err, "servicemonitor: unable to get object")
+		}
+
+		err = ctx.Client.Delete(ctx.Context, existing)
+		if err != nil {
+			return components.Result{}, errors.Wrap(err, "servicemonitor: unabled to delete object")
+		}
 		return components.Result{}, nil
 	}
 
