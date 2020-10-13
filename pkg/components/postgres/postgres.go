cdb29b06d86da0ecae7c247e05750923a53746e2
vivekjainx86 <vivekjainx86@gmail.com>
2020-10-13 18:26:42 +0530

    fix db connection kroken issue

--- pkg/components/postgres/postgres.go
+++ pkg/components/postgres/postgres.go
@@ -43,9 +43,23 @@ func Open(ctx *components.ComponentContext, dbInfo *dbv1beta1.PostgresConnection
 		sslmode = dbInfo.SSLMode
 	}
 	connStr := fmt.Sprintf("host=%s port=%v dbname=%s user=%v password='%s' sslmode=%s", dbInfo.Host, uint16(port), dbInfo.Database, dbInfo.Username, dbPassword, sslmode)
-	db, err := dbpool.Open("postgres", connStr)
-	if err != nil {
-		return nil, errors.Wrap(err, "database: Unable to open database connection")
+
+	// retry creating DB connection object 3 times if there connection object is expired or broken
+	var count int
+	for retry := 0; retry < 3; retry++ {
+		db, err := dbpool.Open("postgres", connStr)
+		if err != nil {
+			return nil, errors.Wrap(err, "database: Unable to open database connection")
+		}
+		// Test db connection
+		row := db.QueryRow("SELECT 1")
+		err = row.Scan(&count)
+		if err == nil {
+			return db, nil
+		}
+		// delete connection object from sync map
+		dbpool.Dbs.Delete(fmt.Sprintf("postgres %s", connStr))
 	}
-	return db, nil
+
+	return nil, err
 }
