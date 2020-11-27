c2eb3cb9d0c52c3dd0a08728ea252733ef92bab1
vivekjainx86 <vivekjainx86@gmail.com>
2020-11-27 15:41:13 +0530

    import k8serror

--- pkg/controller/summon/components/deployment.go
+++ pkg/controller/summon/components/deployment.go
@@ -26,6 +26,7 @@ import (
 	"github.com/pkg/errors"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 
